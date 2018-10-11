80b0df305980aa4d7b223cba3c1bd6f5c5228fb1
Andrei Gaspar <andrei@ildiesign.com>
2018-10-11 14:54:08 +0300

    - Changed datatype ids

--- src/constants/DataTypes.ts
+++ src/constants/DataTypes.ts
@@ -5,10 +5,10 @@
 
 export class DataTypes {
 
-    public static readonly BOOLEAN: number = 1;
+    public static readonly TEXT: number = 1;
     public static readonly NUMBER: number = 2;
-    public static readonly DATE: number = 3;
-    public static readonly TEXT: number = 4;
-    public static readonly LOOKUP: number = 5;
+    public static readonly LOOKUP: number = 3;
+    public static readonly DATE: number = 4;
+    public static readonly BOOLEAN: number = 5;
 
 }
