6e216510fc9463bfbf6d4ffd48e7bf4f1360bd2b
Andrei Gaspar <andrei@ildiesign.com>
2018-10-18 16:14:01 +0300

    - Edit object types feature

--- src/controllers/ObjectTypeController.ts
+++ src/controllers/ObjectTypeController.ts
@@ -3,7 +3,10 @@
 import { Router, Request, Response, NextFunction } from "express";
 
 import ObjectType from "../models/ObjectType";
+import Object from "../models/Object";
 import PropertyDef from "../models/PropertyDef";
+import {IObjectType} from "../models/interfaces/IObjectType";
+import {IPropertyDef} from "../models/interfaces/IPropertyDef";
 
 
 
@@ -33,6 +36,7 @@ class ObjectTypeController {
     public getObjectTypes(req: Request, res: Response, next: NextFunction) {
 
         ObjectType.find()
+            .populate( "properties" )
             .then( (objectTypes) => res.send( { success: true, objectTypes } ) )
             .catch( next );
 
@@ -56,6 +60,7 @@ class ObjectTypeController {
 
         const propertyIds = properties.map( p => p.id );
         const requiredProperties = properties.filter( p => p.required === true ).map( p => p.id );
+        requiredProperties.push( nameProperty );
 
         const objectType = new ObjectType({
             name,
@@ -87,18 +92,85 @@ class ObjectTypeController {
 
         const { id, name, nameProperty, properties } = req.body;
 
+        const newProperties = properties.map( p => p.id );
+        let oldProperties = [];
+        let removedProperties = [];
+        let addedProperties = [];
+        let requiredProperties = [];
+        let notRequiredProperties = [];
+
         ObjectType.findById( id )
             .populate( "properties" )
             .then( (objectType) => {
-                objectType.name = name;
-                objectType.nameProperty = nameProperty;
 
-                //TODO: find out what propertyDefs changed to required/not required
-                //TODO: update the properties accordingly
-                //TODO: find out which are the new properties/no longer in use
-                //TODO: update the objects accordingly
+                objectType.name             = name;
+                objectType.nameProperty     = nameProperty;
+                objectType.properties       = newProperties;
+
+                oldProperties               = objectType.properties.map( p => (p as any)._id.toString() );
+
+                removedProperties           = oldProperties.filter( p => newProperties.indexOf( p ) === -1 );
+
+                addedProperties             = newProperties.filter( p => oldProperties.indexOf( p ) === -1 );
+
+                requiredProperties          = properties.filter( p => (p as any).required && (p as any).id !== nameProperty ).map( p => p.id );
+                requiredProperties.push( nameProperty );
+
+
+                notRequiredProperties   = properties.filter( p => ! (p as any).required ).map( p => p.id );
+
+                for ( let p of removedProperties ) {
+                    if ( notRequiredProperties.indexOf( p ) === -1 ) notRequiredProperties.push( p );
+                }
+
+
+                PropertyDef.update({ _id: { $in: notRequiredProperties } }, { $pull: { requiredFor: id } }, { multi: true } );
+
+
+                return Promise.all([
+                    objectType.save(),
+                    PropertyDef.update({ _id: { $in: notRequiredProperties } }, { $pull: { requiredFor: id } }, { multi: true } ),
+                    PropertyDef.update({ _id: { $in: requiredProperties } }, { $addToSet: { requiredFor: id } }, { multi: true } )
+                ])
             })
+            .then( () => {
+                return Promise.all([
+                    Object.find( { type: id } ) as any,
+                    PropertyDef.find( { _id: { $in: addedProperties } } )
+                ])
+            })
+            .then( (results) => {
 
+                const objects       = results[0];
+                const propertyDefs  = results[1];
+
+
+                return objects.map( (object) => {
+
+                    for ( let property of object.properties ) {
+
+                        if ( removedProperties.indexOf( property.propertyDef ) > -1 ) {
+                            object.properties.$pull( property._id );
+                        }
+                    }
+
+                    for ( let addProp of propertyDefs ) {
+                        object.properties.push({
+                            name: addProp.name,
+                            dataType: addProp.dataType,
+                            propertyDef: addProp._id
+                        })
+                    }
+
+                    return object.save();
+
+                })
+            })
+            .then( (promises) => {
+                return Promise.all( promises );
+            })
+            .then( (results) => res.send( { success: true, objectsUpdated: results.length } ) )
+            .catch( next );
 
     }
 
