187f3cc50a6fc5b3c6f0153599be560cbd278fec
Andrei Gaspar <andrei@ildiesign.com>
2018-10-25 09:35:30 +0300

    - Delete property definitions

--- src/controllers/PropertyDefController.ts
+++ src/controllers/PropertyDefController.ts
@@ -4,6 +4,8 @@ import { Router, Request, Response, NextFunction } from "express";
 
 import PropertyDef from "../models/PropertyDef";
 import Object from "../models/Object";
+import ObjectType from "../models/ObjectType";
+import {IObjectType} from "../models/interfaces/IObjectType";
 
 
 
@@ -50,8 +52,59 @@ class PropertyDefController {
     public deletePropertyDef(req: Request, res: Response, next: NextFunction) {
         const propertyDefId: string = req.params.id;
 
-        PropertyDef.findByIdAndRemove( propertyDefId )
-            .then( () => res.send( { success: true, message: "Property definition successfully deleted." } ) )
+        ObjectType.find( { nameProperty: propertyDefId } )
+            .then( (objectTypes): any => {
+
+                let otNames: string = "";
+
+                for ( let ot of objectTypes ) {
+                    otNames += ot.name + " ";
+                }
+
+
+                if ( objectTypes.length ) {
+                    res.send( { success: false, message: "Property definition is used as the name property for " + otNames + " object types. Therefore it cannot be deleted" } );
+                    return;
+                } else {
+
+                    return ObjectType.find( { properties: propertyDefId } );
+
+                }
+
+            })
+            .then( (objectTypes): any => {
+
+                let objectTypeIds = [];
+
+                for ( let ot of objectTypes ) {
+                    objectTypeIds.push( ot._id );
+
+                    ot.properties.pull( propertyDefId );
+                }
+
+                return Object.find( { type: { $in: objectTypeIds } } );
+            })
+            .then( (objects)=> {
+
+                return objects.map( (object) => {
+
+                    for ( let property of object.properties ) {
+
+                        if ( property.propertyDef.toString() === propertyDefId ) {
+                            console.log( "Property found and removed on object." );
+                            object.properties.pull( property._id );
+                            break;
+                        }
+
+                    }
+
+                    return object.save();
+                })
+            })
+            .then( (promises) => {
+                return Promise.all( promises );
+            })
+            .then( (results) => res.send( { success: true, message: "PropertyDef deletion successful, number of objects updated: " + results.length } ) )
             .catch( next );
     }
 
